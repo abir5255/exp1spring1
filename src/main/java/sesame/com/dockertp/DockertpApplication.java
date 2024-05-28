02b0ed1352e9ff49a97680b9d951dfae43891645
abir5255 <155469727+abir5255@users.noreply.github.com>
2024-05-28 15:10:25 +0100

    DockertpApplication.java

--- src/main/java/sesame/com/dockertp/DockertpApplication.java
+++ src/main/java/sesame/com/dockertp/DockertpApplication.java
@@ -10,14 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DockertpApplication {
-
+EtudiantRepository etdRepository;
+	public ApiEtudiantApplication(EtudiantRepository etdRepository) {
+	super();
+	this.etdRepository = etdRepository;
+}
 	public static void main(String[] args) {
 		SpringApplication.run(DockertpApplication.class, args);
 	}
-@GetMapping("/test")
-public String hello()
-{
-	return "hello world";
+@Bean
+	CommandLineRunner start() {
+		return args -> {
+			
+			etdRepository.save(new Etudiant(1L,"Ali","Ali", new Date()));
+			etdRepository.save(new Etudiant(2L,"Asma","Asma", new Date()));
+			etdRepository.save(new Etudiant(3L,"Ahmed","Ahmed", new Date()));
+
+			//customerRepository.findAll().forEach(System.out::println);
+		};
 	}
 }
 
